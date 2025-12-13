25f0856d7a3d3c5eac3c8f1dfb60156ef6c92000
GaelM <109738732+Blaydd89@users.noreply.github.com>
2025-12-13 14:22:57 +0100

    update

---
+++ app/ads-validation/page.tsx
@@ -0,0 +1,11 @@
+// app/ads-validation/page.tsx
+// C'est une page Server Component statique par défaut.
+export default function AdsValidationPage() {
+    return (
+        <div>
+            {/* Petit contenu pour être certain que la page ne soit pas vide */}
+            <h1>Validation Google AdSense</h1>
+            <p>Ce contenu est public pour permettre la vérification du site.</p>
+        </div>
+    );
+}
